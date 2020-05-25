5e5b3e37bbb559f4faba51b49816a5396b0c9025
Alireza <alireza.aali@gmail.com>
2020-05-25 17:37:57 +0430

    Change in program.cs

--- MyFirstConsolApplication/Program.cs
+++ MyFirstConsolApplication/Program.cs
@@ -7,6 +7,7 @@ namespace MyFirstConsolApplication
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("This is amazing, I am starting to learn new developing language");
         }
     }
 }
