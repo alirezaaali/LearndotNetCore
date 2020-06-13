b13780807c3efb9b496bef4ba57d71694ccd031d
Alireza <alireza.aali@gmail.com>
2020-06-13 22:24:52 +0430

    MVCIntro

---
+++ MVCIntro/MVCIntro/Controllers/BlogAdminController.cs
@@ -0,0 +1,39 @@
+﻿using MVCIntro.Models.Blog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCIntro.Controllers
+{
+    public class BlogAdminController : Controller
+    {
+        // GET: BlogAdmin
+        public ActionResult Insert()
+        {
+            return View();
+        }
+
+
+        //[HttpPost]
+        //public ActionResult Save(string Title, string Description, string Link)
+        //{
+        //    var requset = HttpContext.Request.Form;
+
+        //    BlogItem myitem = new BlogItem();
+        //    myitem.Title = requset.Get("Title");
+        //    myitem.Description = requset.Get("Description");
+        //    myitem.Link = requset.Get("Link");
+        //    //save to db
+        //    return View("Insert");
+        //}
+        [HttpPost]
+        public ActionResult Save(BlogItem myItem)
+        {
+
+            //save to db
+            return View("Insert");
+        }
+    }
+}
---
+++ MVCIntro/MVCIntro/Models/Blog/BlogItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCIntro.Models.Blog
+{
+	public class BlogItem
+	{
+		[Display (Name = "تیتر")]
+		public string Title { get; set; }
+		[Display (Name ="لینک")]
+		public string Link { get; set; }
+		[Display (Name = "متن پست")]
+		public string Description { get; set; }
+	}
+}
