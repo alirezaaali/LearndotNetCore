4f3680b394f113cb087eed5438a41ba705f80730
alirezaaali <alireza.aali@gmail.com>
2020-07-19 19:55:15 +0430

    update990429_endday

---
+++ SOLID_Priciples/SOLID_Priciples/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Priciples
+{
+	class Customer
+	{
+		public int Id { get; set; }
+		public string FName { get; set; }
+		public string LName { get; set; }
+	}
+}
